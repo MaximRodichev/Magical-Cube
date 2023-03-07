bc716b63ef46fb98f68fbae040ee35fbcfa90c3a
Maxim Rodichev <vymasfyaya@gmail.com>
2023-03-07 08:52:28 +0300

    1 практикум

---
+++ Code/ConsoleApp3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1st
+            //    int[] numbers = new int[6];
+            //    Random rand = new Random();
+            //    for (int i = 0; i < numbers.Length; i++)
+            //    {
+            //        numbers[i] = rand.Next(0, 3);
+            //        if (numbers[i] == numbers[0] && i>0)
+            //        {
+            //            Console.WriteLine($"Po index {i} nahoditsya element == pervomy, eto ({numbers[i]} and {numbers[0]})");
+            //        }
+            //    }
+            //    foreach (int i in numbers)
+            //        Console.WriteLine(i);
+            //    Console.WriteLine("1st task done");
+            //    Console.ReadLine();
+
+            //// 2nd task
+            //int[] mass = new int[10];
+            //Random random = new Random();
+            //for (int i = 0; i < mass.Length; i++)
+            //    mass[i] = random.Next(-100,200);
+
+            //Console.WriteLine("All non negative numbers");
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    if (mass[i] > 0)
+            //    {
+            //        Console.WriteLine(mass[i]);
+            //    }
+            //}
+            //Console.WriteLine("All numbers below than 100");
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    if (mass[i] < 100)
+            //    {
+            //        Console.WriteLine(mass[i]);
+            //    }
+            //}
+            //Console.WriteLine("All 2n numbers");
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    if (mass[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine(mass[i]);
+            //    }
+            //}
+            //Console.WriteLine("task 2 end");
+
+            ////3rd
+            //int[] num = { 5, -3, 4, -8, -9, 1, 9, 6, -3, 4, 8, 2 };
+            //int iter=0;
+            //for(int i = 0; i < num.Length; i++)
+            //    if (num[i] < 0)
+            //        iter += 1;
+
+            //Console.WriteLine($"In mass {iter} negatibe numbers");
+            //Console.WriteLine("task 3 end");
+
+            ////4th task
+            //int[] mass = new int[10];
+            //Random random = new Random();
+            //int sum = 0;
+            //int iter = 0;
+            //for (int i = 0; i < mass.Length; i++)
+            //{ mass[i] = random.Next(-10, 10);
+            //    if (mass[i] < 0)
+            //        iter++;
+            //        sum += mass[i];
+            //        }
+            //foreach (int i in mass)
+            //    Console.WriteLine(i);
+            //Console.WriteLine($"Summ of elements it {sum}\nTotal numbers - {iter}\nAverage == {(double)sum/iter:f2}");
+
+            ////5th
+            //int[] mass = new int[10];
+            //Random random = new Random();
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] = random.Next(-10, 10);
+            //    Console.WriteLine($"Old == {mass[i]}\nNew =={mass[i]*5}" );
+            //    mass[i] = mass[i] * 5;
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Code/Я УСТАЛ ПОМОГИТЕ ПРОШУ УМОЛЯЮ/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Я_УСТАЛ_ПОМОГИТЕ_ПРОШУ_УМОЛЯЮ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1st Task
+            int[] home = new int[8];
+            for (int i = 0; i < home.Length; i++)
+            {
+                Console.Write($"home[{i}] == ");
+                home[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Your array!");
+            foreach (int i in home)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("End");
+            Console.ReadLine();
+
+            //2nd Task
+            int n = 10;
+            float[] mass = new float[n];
+            Random rand = new Random();
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rand.Next(-100, 100);
+            }
+            Console.WriteLine("Array will be generated\nYour massive with only positivi numbers");
+            foreach (var i in mass)
+                if (i > 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            Console.WriteLine("All");
+            Console.ReadLine();
+
+            //3rd task
+            int[] numbers = { -7, 10, 5, 16, -4, 18 };
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"On index {i} mass have == {numbers[i]}");
+            }
+            Console.ReadLine();
+
+            //4th task
+            int k = 12;
+            int[] height = new int[k];
+            Random randHeight = new Random();
+            for (int i = 0; i < height.Length; i++)
+            {
+                height[i] = randHeight.Next(163, 190);
+            }
+            Console.WriteLine($"All Heights of {k} people\n");
+            foreach (int i in height)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("ITS ALL TASKS");
+            Console.ReadLine();
+        }
+    }
+}
